fdb99629cf81dc667c7f53d9124d07b315fabebf
Richard Ressler <rressler@american.edu>
2021-02-22 19:24:37 -0500

    step 12

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,14 @@
+#' Combine levels of two factors
+#'
+#' This creates a new factor with the concatenated levels of the two input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return a factor with combined levels
+#' @export fbind
+#' @importFrom ggplot2 ggplot aes geom_bar coord_flip
+#' @examples fbind(as.factor(c("dog","cat")),as.factor(c("gerbil","parakeet")))
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
